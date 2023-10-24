eb3a76df454e5994095ce2308db07fb0810e6ce6
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-10-25 10:41:48 +1100

    add

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Nav from '../components/nav'
+import "./about.css"; // Import the CSS file
 
 const About = () => {
   return (
     <div>
+      <Nav/>
       
     </div>
   )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Nav from '../components/nav'
+import "./contact.css"; // Import the CSS file
 
 const Contact = () => {
   return (
     <div>
-      
-    </div>
+    <Nav/>
+  
+</div>
   )
 }
 
