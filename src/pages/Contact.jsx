bf2929840c313f4c2572afd6088edf7505161042
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-10-25 11:37:32 +1100

    chanaged

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Nav from '../components/nav'
+import React from "react";
+import Nav from "../components/nav";
 import "./contact.css"; // Import the CSS file
 
 const Contact = () => {
   return (
     <div>
-    <Nav/>
-  
-</div>
-  )
-}
+      <Nav />
+      <div className="page__header">Contact</div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
